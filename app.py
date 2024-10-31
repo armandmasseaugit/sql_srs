b53e34529f3803d8e93cc7bccccebf9d574ac94a
Armand <armandbali23@gmail.com>
2024-10-31 15:01:22 +0100

    modification collègue

--- app.py
+++ app.py
@@ -17,15 +17,26 @@ st.selectbox(
 st.write('Le tableau tableau_df est défini comme tel :')
 
 tableau_df = pd.DataFrame(
-    {'Name': ['John', 'Alice', 'Bob'],
-       'Age': [25, 30, 35],
-       'City': ['New York', 'London', 'Paris']}
+    {'name': ['John', 'Alice', 'Bob'],
+       'age': [25, 30, 35],
+       'city': ['New York', 'London', 'Paris']}
 )#.set_index('Name')
 
-st.dataframe(tableau_df)
+st.write(
+    '''Write an SQL query to only display 
+    the lines of people aged>27yo'''
+)
 
-tab1, tab2, tab3 = st.tabs(['Select','f','t'])
+query = st.text_area(label='Enter your SQL query')
+st.dataframe(db.sql(query))
 
-with tab1:
-    query = st.text_area(label='Entrez votre requête SQL')
-    st.dataframe(db.sql(query))
+tables, solution, tab3 = st.tabs(['Tables', 'Solutions'])
+
+with tables:
+    st.dataframe(tableau_df)
+
+with solution:
+    st.dataframe(
+        tableau_df
+        .query('age>=27')
+    )
