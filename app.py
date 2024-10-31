c770f538c89ae1636929e574d83c9088da9fbbd2
Armand <armandbali23@gmail.com>
2024-10-31 09:41:44 +0100

    mise en ligne de l'app

---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+import duckdb as db
+
+st.write('Le tableau tableau_df est défini comme tel :')
+
+tableau_df = pd.DataFrame(
+    {'Name': ['John', 'Alice', 'Bob'],
+       'Age': [25, 30, 35],
+       'City': ['New York', 'London', 'Paris']}
+)#.set_index('Name')
+
+st.dataframe(tableau_df)
+
+tab1, tab2, tab3 = st.tabs(['Select','f','t'])
+
+with tab1:
+    query = st.text_area(label='Entrez votre requête SQL')
+    st.dataframe(db.sql(query))
