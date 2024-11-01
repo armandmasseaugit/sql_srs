a0e8f7f0b628253f1d87edbccbc03978a9db77bb
Armand <armandbali23@gmail.com>
2024-11-01 16:06:30 +0100

    code clean

--- app.py
+++ app.py
@@ -1,42 +1,54 @@
+# pylint: disable=missing-module-docstring.
 import pandas as pd
 import streamlit as st
 import duckdb as db
 
 tableau_df = pd.DataFrame(
-    {'name': ['John', 'Alice', 'Bob'],
-       'age': [25, 30, 35],
-       'city': ['New York', 'London', 'Paris']}
-)#.set_index('Name')
+    {
+        "name": ["John", "Alice", "Bob"],
+        "age": [25, 30, 35],
+        "city": ["New York", "London", "Paris"],
+    }
+)  # .set_index('Name')
 
-st.write(
-    '''# SQL Space Repetition System'''
-)
+solution_df = db.sql(
+    """SELECT * FROM
+        tableau_df
+        WHERE age>27"""
+).df()
+
+st.write("""# SQL Space Repetition System""")
 
 st.selectbox(
-    label='What would you like to review ?',
-    options=('Join','Group by',
-           'Window functions'),
+    label="What would you like to review ?",
+    options=("Join", "Group by", "Window functions"),
     index=None,
-    placeholder='Select a theme...'
+    placeholder="Select a theme...",
 )
 
-st.write('Le tableau tableau_df est défini comme tel :')
-
+st.write("tableau_df is defined as follow :")
+st.dataframe(tableau_df)
 st.write(
-    '''Write an SQL query to only display 
-    the lines of people aged>27yo'''
+    """Write an SQL query to only display 
+    the lines of people aged>27yo"""
 )
 
-query = st.text_area(label='Enter your SQL query')
-st.dataframe(db.sql(query))
+query = st.text_area(label="Enter your SQL query")
 
-tables, solution = st.tabs(['Tables', 'Solution'])
+result, solution = st.tabs(["Result", "Solution"])
 
-with tables:
-    st.dataframe(tableau_df)
+with result:
+    if query:
+        result_df = db.sql(query).df()
+        st.dataframe(result_df)
+        try:
+            if result_df.compare(solution_df):
+                st.write("Good job !")
+            else:
+                st.write("Wrong ! Try again")
+        except ValueError as e:
+            st.write("Problem")
+
 
 with solution:
-    st.dataframe(
-        tableau_df
-        .query('age>=27')
-    )
+    st.dataframe(solution_df)
